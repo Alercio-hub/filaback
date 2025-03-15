b95aa5cc08630e54ad1cb61dfd88ab05106cbb18
Alercio-hb <git@github.com:Alercio-hub/FilaConnect.git>
2025-03-15 22:26:11 +0200

    new change

--- models/Formador.js
+++ models/Formador.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const FormadorSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   telefone: { type: String, required: true },
-  regiao: { type: String, required: true },  // Remova "required: true" se não for obrigatório
-  precoPorHora: { type: Number, required: true },  // Ou remova "required" se desejar
-  disponibilidade: { type: String, required: true }  // Se deseja armazenar "manha", "tarde", etc.
+  regiao: { type: String, required: true },
+  precoPorHora: { type: Number, required: true },
+  disponibilidade: { type: String, required: true } // Armazena valores como "manha", "tarde", etc.
 });
+
+module.exports = mongoose.model('Formador', FormadorSchema);
